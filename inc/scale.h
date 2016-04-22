17407012cd080e3be73a6b428461f2315cb39d9c
Ne02ptzero <louis@ne02ptzero.me>
2016-04-22 18:22:20 +0200

    Add(YCM file): And some fixes

--- inc/scale.h
+++ inc/scale.h
@@ -6,8 +6,9 @@
 # include <stdio.h>
 # include <stddef.h>
 # include <execinfo.h>
+# include <unistd.h>
 
-# define ERROR(str) printf("\033[1;31mFATAL ERROR:\033[0m 42_scale must stop. \n\033[0;32m> \033[0m %s\n", str); print_trace(); _exit(1);
+# define ERROR(str) printf("\033[1;31mFATAL ERROR:\033[0m 42_scale must stop. \n\033[0;32m> Reason: \033[0m %s\n", str); print_trace(); _exit(1);
 
 /* HELPERS (helpers.c) */
 
