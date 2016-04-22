f958842ceb1770a939adbe16855ec4f49ff74964
Ne02ptzero <louis@ne02ptzero.me>
2016-04-22 18:33:50 +0200

    Fix(Backtrace): Smooth color for bt (useless commit #1)

--- srcs/helpers.c
+++ srcs/helpers.c
@@ -5,10 +5,12 @@ void	print_trace(void) {
 	size_t		size;
 	char		**strings;
 
-	size = backtrace (array, 10);
-	strings = backtrace_symbols (array, size);
-	printf ("\033[0;32m> \033[0m Printing backtrace ...\n");
+	size = backtrace(array, 10);
+	strings = backtrace_symbols(array, size);
+	printf("\033[0;32m> \033[0m Printing backtrace ...\n\e[37m");
+# define NK_IMPLEMENTATION
 	for (size_t i = 0; i < size; i++)
-		printf ("%s\n", strings[i]);
-	free (strings);
+		printf("%s\n", strings[i]);
+	free(strings);
+	printf("\033[0m");
 }
